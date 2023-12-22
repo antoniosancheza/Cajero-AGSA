367537e1d1ee19bfcfa5b0063a6201d91d4e402e
antoniosancheza <antonio.sanchez.alc.gu@gmail.com>
2023-12-22 14:31:56 -0600

    Se actualiza credenciales db para host

--- Modelo/ConexionBD.php
+++ Modelo/ConexionBD.php
@@ -1,7 +1,7 @@
 <?php
 class ConexionBD{
     public function conectorBD(){
-        $conexion = mysqli_connect("localhost", "root", "", "CajeroAGSA")
+        $conexion = mysqli_connect("localhost", "id21689063_root", "Z4f1r0*26", "id21689063_base_datos")
         or die ("Problemas en la conexion a la base de datos");
         return $conexion;
     }
